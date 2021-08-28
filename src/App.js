86bb9155df080b4e846e38af5a51b84c860b3dc8
Kunal J. Pardeshi <kunaljp16@gmail.com>
2021-08-28 22:54:18 +0530

    add expenses component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React from "react";
-import expensesData from '../src/MockData/expenses-data'
-import ExpenseItem from "./Components/ExpensesItem";
+import expensesData from "../src/MockData/expenses-data";
+import Expenses from "./Components/Expenses";
+
 import "./App.scss";
-class App extends React.Component {
-  render() {
-    let expenses = expensesData;
-    return (
-      <div className="container">
-        <h1>This is a React Expenses App</h1>
-        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-      </div>
-    );
-  }
+
+function App() {
+  let expenses = expensesData;
+  return (
+    <div className="container">
+      <h1>This is a React Expenses App</h1>
+      <Expenses item={expenses}/>
+    </div>
+  );
 }
 
 export default App;
